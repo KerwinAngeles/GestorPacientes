aa3e8cd1331eee3b2f6877db30001b87e6126465
KerwinAngeles <kerwinangeles12@gmail.com>
2026-01-27 21:32:42 -0500

    Add password encryption interface and implementation
    Introduces the `IPasswordEncryption` interface for SHA-256 hashing and implements it in the `PasswordHashed` class. This enhances the application's capability to securely handle password encryption.

---
+++ GestorPacientes.Core.Application/Interfaces/Helpers/IPasswordEncryption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorPacientes.Core.Application.Interfaces.Helpers
+{
+    public interface IPasswordEncryption
+    {
+        string ComputeSha256(string password);
+    }
+}
---
+++ GestorPacientes.Infrastructure.Persistence/Provider/PasswordHashed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorPacientes.Infrastructure.Persistence.Provider
+{
+    public class PasswordHashed
+    {
+        public string ComputeSha256(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder sb = new();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    sb.Append(bytes[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
